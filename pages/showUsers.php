020fe2245ff765256546fda75fdb2ea860d47d05
Emran Billah <omegaregions0072004@yahoo.com>
2018-07-14 15:44:20 -0300

    First Commit

--- pages/showUsers.php
+++ pages/showUsers.php
@@ -3,12 +3,12 @@
 <div class="container-fluid padded">
 
   <div class="row">
-    <div class="col-10">
+    <div class="col-lg-10 col-lg-10 col-lg-12 col-lg-12">
       <h4 class="font-two"> Logged in as:
         <b><i class="color-mild"> <?php echo $_SESSION["Username"]; ?> </i></b>
       </h4>
     </div>
-    <div class="col-2">
+    <div class="col-lg-2 col-lg-2 col-lg-12 col-lg-12">
       <button type="button" name="Logout" class="btn btn-primary btn-block bgc-light" onclick="LogoutUser()"> LOGOUT </button>
     </div>
   </div>
